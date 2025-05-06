266741ac47b5039f389d92183c3cae9e1820756d
mottasimsadi <mottasimsadi@gmail.com>
2025-05-07 01:08:21 +0600

    Completed the categories for the left aside section

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -3,27 +3,33 @@ import { Outlet } from 'react-router';
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
 import Navbar from '../components/Navbar';
+import LeftAside from '../components/HomeLayout/LeftAside';
+import RightAside from '../components/HomeLayout/RightAside';
 
 const HomeLayout = () => {
     return (
-        <div>
-            <header>
-                <Header></Header>
-                <section className='w-11/12 mx-auto my-3'>
-                    <LatestNews></LatestNews>
-                </section>
-                <nav className='w-11/12 mx-auto my-3'>
-                    <Navbar></Navbar>
-                </nav>
-            </header>
-            <main>
-                <section className="left-nav"></section>
-                <section className="main">
-                    <Outlet></Outlet>
-                </section>
-                <section className="right-nav"></section>
-            </main>
-        </div>
+      <div>
+        <header>
+          <Header></Header>
+          <section className="w-11/12 mx-auto my-3">
+            <LatestNews></LatestNews>
+          </section>
+          <nav className="w-11/12 mx-auto my-3">
+            <Navbar></Navbar>
+          </nav>
+        </header>
+        <main className="w-11/12 mx-auto my-3 grid grid-cols-12">
+          <aside className='col-span-3'>
+            <LeftAside></LeftAside>
+          </aside>
+          <section className="main col-span-6">
+            <Outlet></Outlet>
+          </section>
+          <aside className='col-span-3'>
+            <RightAside></RightAside>
+          </aside>
+        </main>
+      </div>
     );
 };
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Dragon News Home
+        </div>
+    );
+};
+
+export default Home;
