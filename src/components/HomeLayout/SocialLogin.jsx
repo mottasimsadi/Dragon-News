8585f06a307a01a9dc704ce9745e5b738903772c
mottasimsadi <mottasimsadi@gmail.com>
2025-05-07 01:22:01 +0600

    Updated the social login for the right aside section

--- src/components/HomeLayout/SocialLogin.jsx
+++ src/components/HomeLayout/SocialLogin.jsx
@@ -4,13 +4,17 @@ import { FcGoogle } from 'react-icons/fc';
 
 const SocialLogin = () => {
     return (
-        <div>
-            <h2 className='font-bold mb-5'>Login with</h2>
-            <div className='space-y-3'>
-                <button className='btn btn-outline btn-secondary w-full'><FcGoogle size={24} /> Login with Google</button><FaGithub size={24} />
-                <button className='btn btn-outline btn-primary w-full'> Login with Github</button>
-            </div>
+      <div>
+        <h2 className="font-bold mb-5">Login with</h2>
+        <div className="space-y-3">
+          <button className="btn btn-outline btn-secondary w-full">
+            <FcGoogle size={24} /> Login with Google
+          </button>
+          <button className="btn btn-outline btn-primary w-full">
+            <FaGithub size={24} /> Login with Github
+          </button>
         </div>
+      </div>
     );
 };
 
