951873c1a32bf556fb06d361bc8e8cc9ccb9ced3
mottasimsadi <mottasimsadi@gmail.com>
2025-05-09 03:07:21 +0600

    Completed the login and register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,38 @@
+import { Link } from 'react-router';
+
+const Register = () => {
+    return (
+      <div className="flex justify-center min-h-screen items-center">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+          <h2 className="font-semibold text-2xl text-center">
+            Register your account
+          </h2>
+          <div className="card-body">
+            <fieldset className="fieldset">
+              {/* Name */}
+              <label className="label">Your Name</label>
+              <input type="text" className="input" placeholder="Enter you name" />
+              {/* Photo URL */}
+              <label className="label">Photo URL</label>
+              <input type="text" className="input" placeholder="Enter your photo url" />
+              {/* Email */}
+              <label className="label">Email</label>
+              <input type="email" className="input" placeholder="Enter your email address" />
+              {/* Password */}
+              <label className="label">Password</label>
+              <input type="password" className="input" placeholder="Enter your password" />
+              <button className="btn btn-neutral mt-4">Register</button>
+              <p className="font-semibold text-center pt-5">
+                Already Have An Account?{" "}
+                <Link className="text-secondary" to="/auth/login">
+                  Login
+                </Link>
+              </p>
+            </fieldset>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Register;
